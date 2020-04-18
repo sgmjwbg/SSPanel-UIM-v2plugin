8cc16717bec24767089936f27b06d05d6fc07470
sgmjwbg <61115527+sgmjwbg@users.noreply.github.com>
2020-04-19 03:56:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -168,7 +168,7 @@ pre_install_docker_compose(){
 
     echo "Which docker image address will be used"
     read -p "(image address (Default sk9527sk9527/v2ray_v3:go_pay):" docker_addresss
-    [ -z "${docker_addresss}" ] && docker_addresss="hulisang/v2ray_v3:go_pay"
+    [ -z "${docker_addresss}" ] && docker_addresss="sk9527sk9527/v2ray_v3:go_pay"
     echo
     echo "---------------------------"
     echo "docker_addresss = ${docker_addresss}"
@@ -475,7 +475,7 @@ services:
         max-file: "3"
 
   caddy:
-    image: hulisang/v2ray_v3:caddy
+    image: sk9527sk9527/v2ray_v3:caddy
     restart: always
     environment:
       - ACME_AGREE=true
@@ -560,7 +560,7 @@ services:
         max-file: "3"
 
   caddy:
-    image: hulisang/v2ray_v3:caddy
+    image: sk9527sk9527/v2ray_v3:caddy
     restart: always
     environment:
       - ACME_AGREE=true
